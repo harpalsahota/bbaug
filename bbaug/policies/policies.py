b528fb222ed11caaef846a651caef7056b560bbf
Harpal Sahota <harpal28sahota@gmail.com>
2020-03-25 17:48:15 +0000

    added doc strings

--- bbaug/policies/policies.py
+++ bbaug/policies/policies.py
@@ -6,6 +6,8 @@ Ref: https://github.com/tensorflow/tpu/blob/2264f53d95852efbfb82ea27f03ca749e120
 from collections import namedtuple
 import random
 from typing import (
+    Callable,
+    Dict,
     List,
     NamedTuple,
     Tuple,
@@ -27,7 +29,7 @@ def policies_v3() -> List[List[POLICY_TUPLE_TYPE]]:
     """
     Version 3 of augmentation policies
 ​
-    :rtype: List[Tuple[str, float, int]]
+    :rtype: List[List[POLICY_TUPLE_TYPE]]
     :return: List of policies
     """
     policy = [
@@ -96,16 +98,62 @@ def policies_v3() -> List[List[POLICY_TUPLE_TYPE]]:
 
 
 class PolicyContainer:
+    """
+    Policy container for all the policies available during augmentation
+    """
 
-    def __init__(self, policy_list, name_to_augmentation=NAME_TO_AUGMENTATION, return_yolo=False):
+    def __init__(
+            self,
+            policy_list: List[List[POLICY_TUPLE_TYPE]],
+            name_to_augmentation: Dict[str, Callable]=NAME_TO_AUGMENTATION,
+            return_yolo=False
+    ):
+        """
+        Policy container initialisation
+
+        :type policy_list: List[List[POLICY_TUPLE_TYPE]]
+        :param policy_list: List of policies available for augmentation
+        :type name_to_augmentation: Dict[str, Callable]
+        :param name_to_augmentation: Mapping of augmentation name to function
+                                     reference
+        :type return_yolo: bool
+        :param return_yolo: Flag for returning the bounding boxes in YOLO
+                            format
+        """
         self.policies = policy_list
         self.augmentations = name_to_augmentation
         self.return_yolo = return_yolo
 
-    def __getitem__(self, item):
+    def __getitem__(self, item: str) -> Callable:
+        """
+        Returns the augmentation method reference
+
+        :type item: str
+        :param item: Name of augmentation method
+        :rtype Callable
+        :return: Augmentation method
+        """
         return self.augmentations[item]
 
-    def _bbs_to_percent(self, bounding_boxes, image_height, image_width):
+    def _bbs_to_percent(
+            self,
+            bounding_boxes: List[BoundingBox],
+            image_height: int,
+            image_width: int,
+    ) -> np.array:
+        """
+        Convert the augmented bounding boxes to YOLO format:
+        [x_centre, y_centre, box_width, box_height]
+
+        :type bounding_boxes: List[BoundingBox]
+        :param bounding_boxes: list of augmented bounding boxes
+        :type image_height: int
+        :param image_height: Height of the image
+        :type image_width: int
+        :param image_width: Width of the image
+        :rtype: np.array
+        :return: Numpy array of augmented bounding boxes
+        """
         return np.array([
             [
                 bb.center_x / image_width,
@@ -116,7 +164,16 @@ class PolicyContainer:
             for bb in bounding_boxes
         ])
 
-    def _bbs_to_pixel(self, bounding_boxes):
+    def _bbs_to_pixel(self, bounding_boxes: List[BoundingBox]) -> np.array:
+        """
+        Return the augmented bounding boxes in pixel format:
+        [x_min, y_min, x_max, y_max]
+
+        :type bounding_boxes: List[BoundingBox]
+        :param bounding_boxes:
+        :rtype: np.array
+        :return: Numpy array of augmented bounding boxes
+        """
         return np.array([
             [
                 bb.x1,
@@ -127,10 +184,34 @@ class PolicyContainer:
             for bb in bounding_boxes
         ]).astype('int32')
 
-    def select_random_policy(self):
+    def select_random_policy(self) -> List[POLICY_TUPLE]:
+        """
+        Selects a random policy from the list of available policies
+
+        :rtype: List[POLICY_TUPLE]
+        :return: Randomly selected policy
+        """
         return random.choice(self.policies)
 
-    def apply_augmentation(self, policy, image, bounding_boxes):
+    def apply_augmentation(
+            self,
+            policy: List[POLICY_TUPLE],
+            image: np.array,
+            bounding_boxes: List[List[int, int, int, int]]
+    ) -> Tuple[np.array, np.array]:
+        """
+        Applies the augmentations to the image.
+
+        :type policy: List[POLICY_TUPLE]
+        :param policy: Augmentation policy to apply to the image
+        :type image: np.array
+        :param image: Image to augment
+        :type bounding_boxes: List[List[int, int, int, int]]
+        :param bounding_boxes: Bounding boxes for the image in the format:
+                               [x_min, y_min, x_max, y_max]
+        :rtype: Tuple[np.array, np.array]
+        :return: Tuple containing the augmented image and bounding boxes
+        """
         bbs = BoundingBoxesOnImage(
             [BoundingBox(*bb) for bb in bounding_boxes],
             image.shape
