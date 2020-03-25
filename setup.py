017502db892f761e979e918b1f0cd110c6288849
Harpal Sahota <harpal28sahota@gmail.com>
2020-03-25 19:03:25 +0000

    inital commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+description = "Bounding box augmentation for PyTorch with Google's Brain Team augmentation policies"
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='bbaug',
+    version='0.1.0-beta.0',
+    author='Harpal Sahota',
+    author_email='harpal28sahota@gmail.com',
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/harpalsahota/bbaug',
+    packages=['bbaug'],
+    python_requires='>=3.5',
+    install_requires=[
+        'imgaug',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering :: Image Recognition',
+    ]
+)
