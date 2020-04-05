687dac7241b7af0deac26a7786d08866f25a4650
Harpal Sahota <harpal28sahota@gmail.com>
2020-04-05 16:22:10 +0100

    finalised method for visualising a policy

--- bbaug/visuals/visuals.py
+++ bbaug/visuals/visuals.py
@@ -1,6 +1,8 @@
 """ Module to visualise policies """
 
 from typing import (
+    Callable,
+    Dict,
     List
 )
 
@@ -12,60 +14,52 @@ from imgaug.augmentables.bbs import (
 import matplotlib.pyplot as plt
 
 from bbaug.augmentations import NAME_TO_AUGMENTATION
-from bbaug.policies import POLICY_TUPLE_TYPE
+from bbaug.policies import POLICY_TUPLE_TYPE, PolicyContainer
 
-__all__ = ['visualise_policy']
-
-
-def _aug_to_function(augmentation: POLICY_TUPLE_TYPE):
-    """
-    Obtain the method to apply the augmentation
-
-    :type augmentation: POLICY_TUPLE_TYPE
-    :param augmentation: Single augmentation
-    :rtype: Function
-    :return: Method to apply the augmentation
-    """
-    return NAME_TO_AUGMENTATION[augmentation.name](augmentation.magnitude)
+__all__ = [
+    'visualise_policy'
+]
 
 
 def visualise_policy(
         image_path: str,
+        save_path: str,
         bounding_boxes: List[List[int]],
-        policy: List[POLICY_TUPLE_TYPE],
+        policy: List[List[POLICY_TUPLE_TYPE]],
+        name_to_augmentation: Dict[str, Callable]=NAME_TO_AUGMENTATION
 ) -> None:
     """
     Visualise a single policy on an image
 
     :type image_path: str
     :param image_path: Path of the image
+    :type save_path: str
+    :param save_path: Directory where to save the images
     :type bounding_boxes: List[List[int]]
     :param bounding_boxes: Bounding boxes for the image
-    :type policy: List[POLICY_TUPLE_TYPE]
-    :param policy: Single policy to apply to image
+    :type policy: List[List[POLICY_TUPLE_TYPE]]
+    :param policy: The policy set to apply to the image
+    :type name_to_augmentation: Dict[str, Callable]
+    :param name_to_augmentation: Dictionary mapping of the augmentation name
+                                 to the augmentation method
     :rtype: None
     """
-    font_dict = {'fontsize': 9, 'fontweight': 'medium'}
-    fig, axes = plt.subplots(1, 3, figsize=(15, 4))
-    image = imageio.imread(image_path)
-    bbs = BoundingBoxesOnImage([
-        BoundingBox(*box)
-        for box in bounding_boxes
-    ], shape=image.shape)
-    [ax.axis('off') for ax in axes]
+    policy_container = PolicyContainer(
+        policy,
+        name_to_augmentation=name_to_augmentation
+    )
 
-    img_aug, bbs_aug = _aug_to_function(policy[0])(image=image, bounding_boxes=bbs)
-    axes[0].imshow(bbs_aug.draw_on_image(img_aug, size=2))
-    axes[0].set_title(policy[0], fontdict=font_dict)
-
-    img_aug, bbs_aug = _aug_to_function(policy[1])(image=image, bounding_boxes=bbs)
-    axes[1].imshow(bbs_aug.draw_on_image(img_aug, size=2))
-    axes[1].set_title(policy[1], fontdict=font_dict)
-
-    img_aug, bbs_aug = _aug_to_function(policy[0])(image=image, bounding_boxes=bbs)
-    img_aug, bbs_aug = _aug_to_function(policy[1])(image=img_aug, bounding_boxes=bbs_aug)
-    axes[2].imshow(bbs_aug.draw_on_image(img_aug, size=2))
-    axes[2].set_title(f'{policy[0]}\n{policy[1]}', fontdict=font_dict)
-
-    fig.tight_layout()
-    plt.show()
+    for i, pol in enumerate(policy):
+        fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+        image = imageio.imread(image_path)
+        [ax.axis('off') for ax in axes]
+        for ax in range(3):
+            img_aug, bbs_aug = policy_container.apply_augmentation(pol, image, bounding_boxes)
+            bbs_aug = BoundingBoxesOnImage([
+                BoundingBox(*box)
+                for box in bbs_aug
+            ], shape=image.shape)
+            axes[ax].imshow(bbs_aug.draw_on_image(img_aug, size=2))
+        fig.suptitle(pol)
+        fig.tight_layout()
+        fig.savefig(f'{save_path}/sub_policy_{i}.png')
