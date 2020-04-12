f5fccbecb7a554a841d61a774d8f1bd2d74254f2
Harpal Sahota <harpal28sahota@gmail.com>
2020-04-12 16:44:13 +0100

    Bump version: 0.3.0 → 0.4.0-beta.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='bbaug',
-    version='0.3.0',
+    version='0.4.0-beta.0',
     author='Harpal Sahota',
     author_email='harpal28sahota@gmail.com',
     description=description,
